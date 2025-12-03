86791c0a5cec504c23cf1ec5f0488b25a52af3e6
Admin <konrad20050203@gmail.com>
2025-12-04 00:15:40 +0100

    polepszenie nazw xpp

---
+++ maze_step_by_step_visualization.py
@@ -0,0 +1,93 @@
+import pygame
+import sys
+import maze_generators.Binary_tree_maze as bt
+import maze_generators.Hunt_and_kill as hak
+import maze_generators.converter as ct
+import time
+import Visuals.utility_functions as utility
+
+def start(maze, path):
+    pygame.init()
+
+    # FULLSCREEN
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    WIDTH, HEIGHT = screen.get_size()
+    pygame.display.set_caption("Maze Generator")
+
+    # Zegar bierzemy, żeby kontrolować framerate, ale na razie nie używam
+    # clock = pygame.time.Clock()
+
+    CELL_SIZE = 60
+
+    #rzeczy do kamery
+    last_mouse_pos = pygame.mouse.get_pos()
+    x, y = 200, 200
+    dragging = False
+    zoom = 1
+
+    i = 0
+    visited = []
+
+    # Main Game Loop
+    running = True
+    while running:
+
+        #eventy
+        for event in pygame.event.get():
+            # wylaczanie escapem dla pelnego ekranu
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+            # wylaczanie zwykle lel
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                last_mouse_pos = pygame.mouse.get_pos()
+                dragging = True
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                dragging = False
+
+            if event.type == pygame.MOUSEWHEEL:
+                if event.y > 0:
+                    zoom *= 1.1
+                    zoom = min(4, zoom)
+                else:
+                    zoom /= 1.1
+                    zoom = max(0.1, zoom)
+
+        if dragging:
+            mx, my = pygame.mouse.get_pos()
+            dx = mx - last_mouse_pos[0]
+            dy = my - last_mouse_pos[1]
+            x += dx
+            y += dy
+            last_mouse_pos = (mx, my)
+
+        screen.fill((150,150,150))
+
+        #rysujemy maze
+        cur = path[i]
+        visited.append(cur)
+        utility.draw_visited(screen,maze, zoom, CELL_SIZE, cur, visited, x, y)
+
+        #rysowanie maze krok po kroku
+
+        pygame.display.flip()
+        time.sleep(0.1)
+        i += 1
+        if i >= len(path):
+            i = 0
+            visited = []
+            time.sleep(1)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    n, m = 8, 8
+    maze, path = hak.generate_maze(n, m)
+    maze = hak.maze_convert(maze)
+    start(maze, path)
