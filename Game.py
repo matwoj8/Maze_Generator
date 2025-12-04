e2459fb34a160a5906b6d7ed6d39a6b0cdb57820
Admin <konrad20050203@gmail.com>
2025-12-04 10:27:43 +0100

    proste menu gry, wybor postaci

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ import maze_generators.mazes.Hunt_And_Kill_Maze as hak
 import maze_generators.mazes.Binary_Tree_Maze as bt
 import maze_generators.converter as ct
 import maze_generators as mg
+import Player.character as character
 
 def start(CELL_SIZE: int) -> None:
     pygame.init()
@@ -15,7 +16,8 @@ def start(CELL_SIZE: int) -> None:
     pygame.display.set_caption("Maze Generator")
 
     last_mouse_pos = pygame.mouse.get_pos()
-    x, y = 0, 0
+    x, y = 100, 50
+    sx, sy = 0, 0
     dragging = False
     zoom = 1
 
@@ -24,12 +26,10 @@ def start(CELL_SIZE: int) -> None:
     generated = False
 
     font = pygame.font.SysFont("comicsans", 30)
-    #obecne stany = ["menu", "generate", "draw_maze", "step_by_step_visualization"]
+    #obecne stany = ["menu", "character_selection", "game"]
     game_state = "menu" # zaczynamy w menu
 
-    input_box = pygame.Rect(200, 100, 200, 50)
-    input_text = ""
-    active = False
+    chosen_character = ""
 
     running = True
     while running:
@@ -44,7 +44,7 @@ def start(CELL_SIZE: int) -> None:
             if event.type == pygame.QUIT:
                 running = False
 
-            if game_state == "draw_maze" or game_state == "step_by_step_visualization":
+            if game_state == "game":
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     last_mouse_pos = pygame.mouse.get_pos()
                     dragging = True
@@ -60,65 +60,54 @@ def start(CELL_SIZE: int) -> None:
                         zoom /= 1.1
                         zoom = max(0.1, zoom)
 
-            if game_state == "generate":
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    active = input_box.collidepoint(event.pos)
-
-                if event.type == pygame.KEYDOWN and active:
-                    if event.key == pygame.K_BACKSPACE:
-                        input_text = input_text[:-1]
-                    else:
-                        input_text += event.unicode
 
         screen.fill((255, 255, 255))
 
         if game_state == "menu":
-            if utility.draw_button(screen, "Maze generator", WIDTH/2-600, HEIGHT/2, 300, 150, font=font):
-                game_state = "generate"
-        elif game_state == "generate":
+            if utility.draw_button(screen, "Maze generator", WIDTH/2-150, HEIGHT/2-75, 300, 150, font=font):
+                game_state = "character_selection"
+                time.sleep(0.1)
+        elif game_state == "character_selection":
+            if utility.draw_button(screen, "Warrior", WIDTH-1820, HEIGHT-800, 500, 600, font=font):
+                game_state = "game"
+                chosen_character = "warrior"
+            if utility.draw_button(screen, "Archer", WIDTH-1210, HEIGHT-800, 500, 600, font=font):
+                game_state = "game"
+                chosen_character = "archer"
+            if utility.draw_button(screen, "Mage", WIDTH-590, HEIGHT-800, 500, 600, font=font):
+                game_state = "game"
+                chosen_character = "mage"
+        elif game_state == "game":
 
-            pygame.draw.rect(screen, (0, 255, 255), input_box, border_radius=5)
-            text_surface = font.render(input_text, True, (0, 0, 0))
-            screen.blit(text_surface, (input_box.x + 10, input_box.y + 10))
+            keys = pygame.key.get_pressed()
 
+            speed = 1
 
-            if utility.draw_button(screen, "Generate Maze", WIDTH/2, HEIGHT/2, 300, 150, font=font):
-                game_state = "draw_maze"
-            if utility.draw_button(screen, "Step by Step Visualization", WIDTH/2, HEIGHT/2-200, 300, 150, font=font):
-                game_state = "step_by_step_visualization"
-        else:
-            if dragging:
-                mx, my = pygame.mouse.get_pos()
-                dx = mx - last_mouse_pos[0]
-                dy = my - last_mouse_pos[1]
-                x += dx
-                y += dy
-                last_mouse_pos = (mx, my)
+            if keys[pygame.K_a]:
+                x -= speed
+            if keys[pygame.K_d]:
+                x += speed
+            if keys[pygame.K_w]:
+                y -= speed
+            if keys[pygame.K_s]:
+                y += speed
 
+            # if dragging:
+            #     mx, my = pygame.mouse.get_pos()
+            #     dx = mx - last_mouse_pos[0]
+            #     dy = my - last_mouse_pos[1]
+            #     x += dx
+            #     y += dy
+            #     last_mouse_pos = (mx, my)
             if not generated:
-                n, m = input_text.split(' ')
-                print(n ,m)
-                maze, path = hak.generate_maze(12, 12)
-                maze = mg.converter.maze_convert(maze)
+                player = character.Character(x, y, chosen_character)
+                maze, path = hak.generate_maze(15, 15)
+                maze = ct.maze_convert(maze)
                 generated = True
 
-            if game_state == "draw_maze":
-                utility.draw_maze(screen, maze, zoom, CELL_SIZE, x, y)
-            elif game_state == "step_by_step_visualization":
-                visited.append(path[i])
-                utility.draw_visited(screen, maze, zoom, CELL_SIZE, path[i], visited, x+100, y+50)
 
-                pygame.display.flip()
-
-                i += 1
-                time.sleep(0.05)
-
-                if i >= len(path):
-                    i = 0
-                    visited = []
-                    time.sleep(1)
-
-            #utility.draw_maze(screen, maze, zoom, CELL_SIZE, x, y)
+            player.update(screen, x, y)
+            utility.draw_maze(screen, maze, zoom, CELL_SIZE, sx, sy)
 
         pygame.display.flip()
 
