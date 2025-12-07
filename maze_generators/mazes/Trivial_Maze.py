27c2e79f4a2a2ee8fa74c5491aa57e9c46c62d66
Mateusz Wójcicki <matwoj8@gmail.com>
2025-12-08 00:24:44 +0100

    ananas

--- maze_generators/mazes/Trivial_Maze.py
+++ maze_generators/mazes/Trivial_Maze.py
@@ -2,11 +2,14 @@ from maze_generators.Maze import *
 
 def generate_maze(m: int, n: int) -> Maze:
     maze = Maze(m, n)
+    path = [(0,0)]
     for i in range(1, m):
+        path.append((i,0))
         actualize_neighbour(maze.board[(i, 0)], 1)
 
     for i in range(m):
         for j in range(1, n):
+            path.append((i,j))
             actualize_neighbour(maze.board[(i, j)], 0)
 
-    return maze
+    return maze, path
